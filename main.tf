1072612956ecd0f027f5e9126275f07801d467ed
naseevadk <101912886+naseevadk@users.noreply.github.com>
2022-06-27 09:59:44 +0545

    Add files via upload
    added main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74.1"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-bata-jenkins-hudai-baneko-instance"
+  }
+}
